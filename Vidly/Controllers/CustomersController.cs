0c2098bfe7ce543d1d01eb33c34b0c1f6a3635cf
Singh, Sushil <sushil.singh@basware.com>
2019-01-11 14:19:55 +0530

    Created Movies & Customers Controller & View

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        IList<Customer> Customers = new List<Customer>();
+
+        public CustomersController()
+        {
+            Customers.Add(new Customer {Id = 1, Name = "Jhon Snow" });
+            Customers.Add(new Customer {Id = 2, Name = "Khalisee" });
+        }
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(Customers);
+        }
+
+        //GET: Customers/Details/1
+        public ActionResult Details(int Id)
+        {
+            Customer customer = Customers.Where(c => c.Id == Id).SingleOrDefault();
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        IList<Movie> Movies = new List<Movie>();
+
+        public MoviesController()
+        {
+            Movies.Add(new Movie { Id = 1, Name = "Shrek!!!" });
+            Movies.Add(new Movie { Id = 2, Name = "Transformers!!!" });
+        }
+
         // GET: Movies
         public ActionResult Index()
         {
-            return View();
+            return View(Movies);
         }
     }
 }
