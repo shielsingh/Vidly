651eb8631ccd0f2c6e6e9c1a2dae99e315fb88fb
Singh, Sushil <sushil.singh@basware.com>
2019-01-25 15:02:18 +0530

    Added default user via entity framework seed method.

--- Vidly/Migrations/Configuration.cs
+++ Vidly/Migrations/Configuration.cs
@@ -1,9 +1,11 @@
 namespace Vidly.Migrations
 {
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Vidly.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Vidly.Models.ApplicationDbContext>
     {
@@ -18,6 +20,26 @@ namespace Vidly.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+
+            IdentityRole role = new IdentityRole() { Id = Guid.NewGuid().ToString(), Name = "SuperAdmin" };
+            ApplicationUser user = new ApplicationUser()
+            {
+                Id = Guid.NewGuid().ToString(),
+                AccessFailedCount = 0,
+                Email = "superadmin@vidly.com",
+                EmailConfirmed = true,
+                LockoutEnabled = false,
+                PasswordHash = "ANLXGxPTH2TTnzz1PyZvlRECuuYkgwuZZDPJeWzAhQtiKXuFFdqYOGzB4zOi8yOv3A==",
+                PhoneNumberConfirmed = false,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                TwoFactorEnabled = false,
+                UserName = "superadmin@vidly.com"
+            };
+            IdentityUserRole userRole = new IdentityUserRole() { RoleId = role.Id, UserId = user.Id };
+            user.Roles.Add(userRole);
+
+            context.Roles.AddOrUpdate(x => x.Id, role);
+            context.Users.AddOrUpdate(x => x.Id, user);
         }
     }
 }
